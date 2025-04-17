fc0d2625ee18f13732af065c1e38a5177e2a6981
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2025-04-17 09:30:21 +0200

    Modify how the csv is compiled

--- pdf_names_to_csv.ps1
+++ pdf_names_to_csv.ps1
@@ -1,10 +1,20 @@
+$fileName = ".\projekty.csv"
+
+# Stworzenie/wyczyszczenie pliku
+Out-File -FilePath $fileName
+
+# Iteracja po wszystkich podkatalogach rekurencyjnie
 Get-ChildItem -Path "." -Directory -Recurse | ForEach-Object {
+    # Tablica z nazwami wszystkich PDFów w danym podkatalogu
     $pdfs = (Get-ChildItem -Path $_.FullName *.pdf).Basename
 
+    # Jak nie jest pusta
     if ($pdfs) {
-        $fileName = "$($_.FullName)\$($_.Name).csv"
-        Out-File -FilePath $fileName
-    
+        # Dodaj do csv nazwę danego podkatalogu
+        $_.FullName | Out-File -Append -FilePath $fileName
+
+        # Dla każdej nazwy pdfa - zastąp podkreślniki przecinkami, po przecinku doklej pełną nazwę na wszelki wypadek,
+        # całość dopisz do csv
         ForEach ($pdf in $pdfs) {
             "$($pdf -split "_" -join ","),$($pdf)" | Out-File -Append -FilePath $fileName
         }
